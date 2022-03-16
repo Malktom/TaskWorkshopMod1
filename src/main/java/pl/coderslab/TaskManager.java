9d76926d161d089c5a4a0c840256ff400109b5f5
Malktom <tomasz.malkiewicz@yahoo.com>
2022-03-16 20:24:52 +0100

    control panel list exit

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,4 +1,59 @@
 package pl.coderslab;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class TaskManager {
+    public static void main(String[] args) {
+
+        viewControlPanel();
+        Scanner scan = new Scanner(System.in);
+
+        Path path1 = Paths.get("tasks.csv");
+
+        while (scan.hasNextLine()) {
+
+            String option = scan.nextLine();
+
+           if ("exit".equals(option)) {      // warunek skonczenia programu - sprawdzamy input poprzez przypisanie go do referencji
+               System.out.println(ConsoleColors.RED + "Bye, bye.");
+               break;
+            }
+            switch (option) {
+                case "add":
+                    System.out.println("add");
+                    break;
+                case "remove":
+                    System.out.println("remove");
+                    break;
+                case "list":
+                    System.out.println("Tasks list:");
+                    try {
+                        for (String line : Files.readAllLines(path1)) {
+                            System.out.println(line);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+            }
+           viewControlPanel();
+
+
+        }
+    }
+
+    private static void viewControlPanel() {
+        System.out.println();
+        System.out.println(ConsoleColors.BLUE + "Please select an option");
+        System.out.println(ConsoleColors.RESET + "add");
+        System.out.println("remove");
+        System.out.println("list");
+        System.out.println("exit");
+        System.out.println();
+    }
 }
