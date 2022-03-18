2c29300c613aab40c64111dd1784e7b5008f3e55
Malktom <tomasz.malkiewicz@yahoo.com>
2022-03-18 01:03:27 +0100

    list - done control panel done add  done remove 0% exit done

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -32,33 +32,23 @@ public class TaskManager {
                     break;
                 case "remove":
                     System.out.println("remove");
-                   // System.out.println(getLinesNumber());
+
                     break;
                 case "list":
                     getList();
                     break;
-//                    System.out.println("Tasks list:");
-//                    try {
-//                        for (String line : Files.readAllLines(path1)) {
-//                            System.out.println(line);
-//                        }
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                    break;
+
                 default:
                     System.out.println("Please select a corect option.");
             }
             viewControlPanel();
-
         }
     }
-
     private static void addTask() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Podaj dane:");
-        String option = scan.nextLine();
-        System.out.println(option);
+        String option = scan.nextLine() + "\n";
+
         Path path1 = Paths.get("tasks.csv");
         try {
             Files.writeString(path1, option, StandardOpenOption.APPEND);   //   JAK DODAWAC WIERSZE OD NOWEJ LINII???
@@ -79,63 +69,54 @@ public class TaskManager {
 
         } catch (FileNotFoundException exception) {
             exception.printStackTrace();
-        }  return lines;
+        }
+        return lines;
     }
-        private static void getList () {
 
+    private static void getList() {
 
-            try {
-                File file = new File("tasks.csv");
-                Scanner scan = new Scanner(file);
-                String[] usersArray = new String[getLinesNumber()];   // tworzymy tablice taskow o wielkosci odpowiadajacej linii tekstu z pliku
+        try {
+            File file = new File("tasks.csv");
+            Scanner scan = new Scanner(file);
+            String[] usersArray = new String[getLinesNumber()];   // tworzymy tablice taskow o wielkosci odpowiadajacej linii tekstu z pliku
 
-                while (scan.hasNextLine()) {
+            while (scan.hasNextLine()) {
 
-                    for (int i = 0; i < getLinesNumber(); i++) {
-                        String task = scan.nextLine();
-                        usersArray[i] = task;                      //ZAPELNIAM TABLICE JEDNOWYMIAROWA LINIAMI Z PLIKU
-                    }
-
-                }
-              // System.out.println(Arrays.toString(usersArray));
-                String[][] taskArray=new String [getLinesNumber()][4];
                 for (int i = 0; i < getLinesNumber(); i++) {
-                        taskArray[i]=usersArray[i].split(",");   //UZUPELNIAM TABLICE 2WYMIAROWA ELEMENTAMII Z KAZDEJ LINII ale bez licznika pozycji
+                    String task = scan.nextLine();
+                    usersArray[i] = task;                      //ZAPELNIAM TABLICE JEDNOWYMIAROWA LINIAMI Z PLIKU
                 }
-               // System.out.println(Arrays.toString(taskArray[1]));
-               // System.out.println(taskArray[1][1]);
-                String[][] numberedTaskArray=new String [getLinesNumber()][4];
-                int licznik=1;
-                for (int i = 0; i < getLinesNumber(); i++) {
-                    for (int j = 0; j < 3;j++) {
-
-                        numberedTaskArray[i][j+1] = taskArray[i][j];
-                        if (j==0) {                                                         // DODAWANIE NUMEROW WIERSZY, dla pozycji 0 dodaje kolejny numer
-                            numberedTaskArray[i][0] = (licznik)+":";
-
-                        }
-                    }licznik++;
-
-                }
-                System.out.println(Arrays.toString(numberedTaskArray[0]));
-                System.out.println(Arrays.toString(numberedTaskArray[1]));
-                System.out.println(Arrays.toString(numberedTaskArray[2]));
-                System.out.println(Arrays.toString(numberedTaskArray[3]));
-            //    System.out.println("lista taskow ponumerowana");
-              //  System.out.println(Arrays.toString(numberedTaskArray[1]));
-
-            } catch (FileNotFoundException exception) {
-                exception.printStackTrace();
             }
-        }
+            String[][] taskArray = new String[getLinesNumber()][4];
+            for (int i = 0; i < getLinesNumber(); i++) {
+                taskArray[i] = usersArray[i].split(",");   //UZUPELNIAM TABLICE 2WYMIAROWA ELEMENTAMII Z KAZDEJ LINII ale bez licznika pozycji
+            }
+            String[][] numberedTaskArray = new String[getLinesNumber()][4];
+            int licznik = 1;
+            for (int i = 0; i < getLinesNumber(); i++) {
+                for (int j = 0; j < 3; j++) {
 
-        private static void viewControlPanel () {
-            System.out.println();
-            System.out.println(ConsoleColors.BLUE + "Please select an option");
-            System.out.println(ConsoleColors.RESET + "add");
-            System.out.println("remove");
-            System.out.println("list");
-            System.out.println("exit");
-            System.out.println();
+                    numberedTaskArray[i][j + 1] = taskArray[i][j];
+                    if (j == 0) {                                                         // DODAWANIE NUMEROW WIERSZY, dla pozycji 0 dodaje kolejny numer
+                        numberedTaskArray[i][0] = (licznik) + ":";
+                    }
+                }
+                licznik++;
+            }
+            for (int i = 0; i < numberedTaskArray.length; i++) {
+                System.out.println(Arrays.toString(numberedTaskArray[i]));
+            }
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
         }
     }
+    private static void viewControlPanel() {
+        System.out.println();
+        System.out.println(ConsoleColors.BLUE + "Please select an option");
+        System.out.println(ConsoleColors.RESET + "add");
+        System.out.println("remove");
+        System.out.println("list");
+        System.out.println("exit");
+        System.out.println();
+    }
+}
